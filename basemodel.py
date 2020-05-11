818a8db4641566b0ac5f9e2775c1d04d833dc8fb
nagareru <49864495+nagareru@users.noreply.github.com>
2020-05-11 18:14:15 +0800

    Update basemodel.py

--- basemodel.py
+++ basemodel.py
@@ -14,6 +14,7 @@ from config import config as cfg
 
 
 @layer_register(log_shape=True)
+#分组归一化（GN）的操作，顺便还集成了bn层的作用
 def GroupNorm(x, group=32, gamma_initializer=tf.constant_initializer(1.)):
     shape = x.get_shape().as_list()
     ndims = len(shape)
@@ -40,7 +41,7 @@ def GroupNorm(x, group=32, gamma_initializer=tf.constant_initializer(1.)):
     out = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-5, name='output')
     return tf.reshape(out, orig_shape, name='output')
 
-
+#感觉是用来处理bn层训练与测试的。测试时冻结bn的参数
 def freeze_affine_getter(getter, *args, **kwargs):
     # custom getter to freeze affine params inside bn
     name = args[0] if len(args) else kwargs.get('name')
@@ -52,7 +53,7 @@ def freeze_affine_getter(getter, *args, **kwargs):
         ret = getter(*args, **kwargs)
     return ret
 
-
+#似乎是用来处理一些padding时的情况
 def maybe_reverse_pad(topleft, bottomright):
     if cfg.BACKBONE.TF_PAD_MODE:
         return [topleft, bottomright]
@@ -60,6 +61,7 @@ def maybe_reverse_pad(topleft, bottomright):
 
 
 @contextmanager
+#没看懂这一堆操作，感觉像是根据网络的状态选择是否冻结参数，根据cfg的设置选择网络中的一些操作细节（
 def backbone_scope(freeze):
     """
     Args:
@@ -89,7 +91,7 @@ def backbone_scope(freeze):
                 stack.enter_context(custom_getter_scope(freeze_affine_getter))
         yield
 
-
+#图像预处理，进行简单的归一化操作（减平均值除标准差）
 def image_preprocess(image, bgr=True):
     with tf.name_scope('image_preprocess'):
         if image.dtype.base_dtype != tf.float32:
@@ -105,7 +107,7 @@ def image_preprocess(image, bgr=True):
         image = (image - image_mean) * image_invstd
         return image
 
-
+#还是有关归一化的一些设置，应该是根据cfg的设置选择norm层名字和具体操作，返回用于norm的函数
 def get_norm(zero_init=False):
     if cfg.BACKBONE.NORM == 'None':
         return lambda x: x
@@ -117,7 +119,7 @@ def get_norm(zero_init=False):
         layer_name = 'bn'
     return lambda x: Norm(layer_name, x, gamma_initializer=tf.zeros_initializer() if zero_init else None)
 
-
+#resnet结构中的短路连接，会处理通道数不相同的情况
 def resnet_shortcut(l, n_out, stride, activation=tf.identity):
     n_in = l.shape[1]
     if n_in != n_out:   # change dimension when channel is not the same
@@ -130,7 +132,7 @@ def resnet_shortcut(l, n_out, stride, activation=tf.identity):
     else:
         return l
 
-
+#resnet50和101中的bottleneck结构，针对不同的模型做出选择具体的连接方式
 def resnet_bottleneck(l, ch_out, stride):
     shortcut = l
     if cfg.BACKBONE.STRIDE_1X1:
@@ -153,7 +155,7 @@ def resnet_bottleneck(l, ch_out, stride):
     ret = l + resnet_shortcut(shortcut, ch_out * 4, stride, activation=get_norm(zero_init=False))
     return tf.nn.relu(ret, name='output')
 
-
+#其实是将上边设计好的残差块（block_func)过上count遍
 def resnet_group(name, l, block_func, features, count, stride):
     with tf.variable_scope(name):
         for i in range(0, count):
@@ -161,7 +163,7 @@ def resnet_group(name, l, block_func, features, count, stride):
                 l = block_func(l, features, stride if i == 0 else 1)
     return l
 
-
+#resnet_c4网络的结构
 def resnet_c4_backbone(image, num_blocks):
     assert len(num_blocks) == 3
     freeze_at = cfg.BACKBONE.FREEZE_AT
@@ -181,12 +183,13 @@ def resnet_c4_backbone(image, num_blocks):
 
 
 @auto_reuse_variable_scope
+#这似乎是单独处理了一块卷积层？感觉是让resnet_c4的输出先经过一些处理再过c5
 def resnet_conv5(image, num_block):
     with backbone_scope(freeze=False):
         l = resnet_group('group3', image, resnet_bottleneck, 512, num_block, 2)
         return l
 
-
+#resnet_fpn网络的结构
 def resnet_fpn_backbone(image, num_blocks):
     freeze_at = cfg.BACKBONE.FREEZE_AT
     shape2d = tf.shape(image)[2:]
