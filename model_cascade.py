12d3e2da1ecb0383a098eb9aab01fa59ed84ccbf
Renrui Zhang <54577425+ZrrSkywalker@users.noreply.github.com>
2020-05-31 13:32:16 +0800

    Update model_cascade.py

--- model_cascade.py
+++ model_cascade.py
@@ -75,6 +75,8 @@ class CascadeRCNNHead(object):
         """
         reg_weights = tf.constant(cfg.CASCADE.BBOX_REG_WEIGHTS[stage], dtype=tf.float32) # 创建cascade的权重，是持久化常量浮点数
         pooled_feature = self.roi_func(proposals.boxes)  # N,C,S,S
+        
+        # FIXME
         if roi_func_extra != None:
              pooled_feature = tf.concate([pooled_feature, self.roi_func_extra(proposals.boxes)], 0)
         pooled_feature = self.scale_gradient(pooled_feature)    # 这里不太理解为什么重新赋值
